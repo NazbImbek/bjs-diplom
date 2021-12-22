0b6bbbab06120c303243aaec44810e195a5d4c32
Kimbaev <kimbaev85@gmail.com>
2021-12-22 23:58:28 +0600

    initial commit

---
+++ public/loginPage.js
@@ -0,0 +1,17 @@
+'use strict'
+const userForm = new UserForm ();
+userForm.loginFormCallback = data => {
+    ApiConnector.login(data, response => {console.log(response);
+    return response.success ? 
+    location.reload() :
+    userForm.setLoginErrorMessage(response.error);
+    });
+};
+
+userForm.registerFormCallback = data => {
+    ApiConnector.register(data, response => {console.log(response);
+    return response.success ? 
+    location.reload() :
+    userForm.setRegisterErrorMessage(response.error);
+    });
+};
